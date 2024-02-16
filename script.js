3f4f3bd53ea6ca404733bd6c2b51a90b89500b70
mark-pherz <marchello.den88@gmail.com>
2024-02-16 16:08:56 +0800

    improve backdrop menu

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const openMenuIcon = document.querySelector('.icon-menu-open');
-openMenuIcon.addEventListener('click', () => {
-  document.querySelector('.nav-mobile').classList.add("active");
-})
+const openMenuIcon = document.querySelector(".icon-menu-open");
+openMenuIcon.addEventListener("click", () => {
+  document.querySelector(".nav-mobile").classList.add("active");
+  document.querySelector(".nav-mobile-backdrop").classList.add("active");
+});
 
-const closeMenuIcon = document.querySelector('.icon-menu-close');
-closeMenuIcon.addEventListener('click', () => {
-  document.querySelector('.nav-mobile').classList.remove("active");
-})
+const closeMenuIcon = document.querySelector(".icon-menu-close");
+closeMenuIcon.addEventListener("click", () => {
+  document.querySelector(".nav-mobile").classList.remove("active");
+  document.querySelector(".nav-mobile-backdrop").classList.remove("active");
+});
